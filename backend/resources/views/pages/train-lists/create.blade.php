e5d612606a557dc633f0d14f549cb65e1b8fe00e
naymur92 <naymurrahman@yahoo.com>
2023-04-01 13:14:07 +0600

    fix view files error showing for root train (train-lists)

--- backend/resources/views/pages/train-lists/create.blade.php
+++ backend/resources/views/pages/train-lists/create.blade.php
@@ -29,17 +29,19 @@
               <div class="form-group">
                 <label for="_name"><strong>Train Name:</strong></label>
                 <input type="text" id="_name" name="train_name" value="{{ old('train_name') }}"
-                  placeholder="Enter Train Name" class="form-control">
+                  placeholder="Enter Train Name" class="form-control @error('train_name') is-invalid @enderror">
 
                 @error('train_name')
-                  <div class="alert alert-warning my-2">{{ $message }}</div>
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span>
                 @enderror
               </div>
 
               <div class="row">
                 <div class="col-6">
                   <label for="_offday"><strong>Select Off-Day:</strong></label>
-                  <select name="off_day" id="_offday" class="form-control">
+                  <select name="off_day" id="_offday" class="form-control @error('off_day') is-invalid @enderror">
                     <option value="" selected disabled>Select One</option>
                     <option value="Saturday" {{ old('off_day') ? 'selected' : '' }}>Saturday</option>
                     <option value="Sunday" {{ old('off_day') ? 'selected' : '' }}>Sunday</option>
@@ -50,18 +52,22 @@
                     <option value="Friday" {{ old('off_day') ? 'selected' : '' }}>Friday</option>
                   </select>
                   @error('off_day')
-                    <div class="alert alert-warning my-2">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
                   @enderror
                 </div>
                 <div class="col-6">
                   <label for="_up_down"><strong>Select Up/Down:</strong></label>
-                  <select name="up_down" id="_up_down" class="form-control">
+                  <select name="up_down" id="_up_down" class="form-control @error('up_down') is-invalid @enderror">
                     <option value="" selected disabled>Select One</option>
                     <option value="0" {{ old('off_day') ? 'selected' : '' }}>Up</option>
                     <option value="1" {{ old('off_day') ? 'selected' : '' }}>Down</option>
                   </select>
                   @error('up_down')
-                    <div class="alert alert-warning my-2">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
                   @enderror
                 </div>
 
--- backend/resources/views/pages/train-lists/edit.blade.php
+++ backend/resources/views/pages/train-lists/edit.blade.php
@@ -32,17 +32,19 @@
                 <label for="_name"><strong>Train Name:</strong></label>
                 <input type="text" id="_name" name="train_name"
                   value="{{ old('train_name', $root_train->train_name) }}" placeholder="Enter Train Name"
-                  class="form-control">
+                  class="form-control @error('train_name') is-invalid @enderror">
 
                 @error('train_name')
-                  <div class="alert alert-warning my-2">{{ $message }}</div>
+                  <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span>
                 @enderror
               </div>
 
               <div class="row">
                 <div class="col-6">
                   <label for="_offday"><strong>Select Off-Day:</strong></label>
-                  <select name="off_day" id="_offday" class="form-control">
+                  <select name="off_day" id="_offday" class="form-control @error('off_day') is-invalid @enderror">
                     <option value="" selected disabled>Select One</option>
                     <option value="Saturday" {{ old('off_day', $root_train->off_day) ? 'selected' : '' }}>Saturday
                     </option>
@@ -58,18 +60,22 @@
                     </option>
                   </select>
                   @error('off_day')
-                    <div class="alert alert-warning my-2">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
                   @enderror
                 </div>
                 <div class="col-6">
                   <label for="_up_down"><strong>Select Up/Down:</strong></label>
-                  <select name="up_down" id="_up_down" class="form-control">
+                  <select name="up_down" id="_up_down" class="form-control @error('up_down') is-invalid @enderror">
                     <option value="" selected disabled>Select One</option>
                     <option value="1" {{ old('off_day', $root_train->up_down) ? 'selected' : '' }}>Up</option>
                     <option value="2" {{ old('off_day', $root_train->up_down) ? 'selected' : '' }}>Down</option>
                   </select>
                   @error('up_down')
-                    <div class="alert alert-warning my-2">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
                   @enderror
                 </div>
 
