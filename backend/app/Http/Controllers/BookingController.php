f6322aee4120bd38d93aebbaa81c2fcc0ee52973
naymur92 <naymurrahman@yahoo.com>
2023-04-02 21:13:55 +0600

    add middleware(auth) constructor on controllers

--- backend/app/Http/Controllers/BookingController.php
+++ backend/app/Http/Controllers/BookingController.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
   public function get_booking_details($id)
   {
     $schedule = Schedule::where('id', $id)->with('train', 'train.bogis', 'train.bogis.bogi_type')->get();
